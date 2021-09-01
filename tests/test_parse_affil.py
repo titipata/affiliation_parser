24b9cc8354c0fca62d28ab5164453aebaca3263b
Li Xing <lix930701@gmail.com>
2021-09-01 13:18:37 +0800

    add pytest files

---
+++ tests/test_parse_affil.py
@@ -0,0 +1,19 @@
+# test parse_affil function
+
+from affiliation_parser import parse_affil
+
+
+def test_parse_affil():
+
+    assert parse_affil(
+        "Department of Health Science, Kochi Women's University, Kochi 780-8515, Japan. watanabe@cc.kochi-wu.ac.jp"
+    ) == {
+        'full_text':
+        "Department of Health Science, Kochi Women's University, Kochi , Japan.",
+        'department': 'Department of Health Science',
+        'institution': "Kochi Women's University",
+        'email': 'watanabe@cc.kochi-wu.ac.jp',
+        'zipcode': '780-8515',
+        'location': 'Kochi , Japan',
+        'country': 'japan'
+    }
